c774935906c2b785250767a60c327b6873c76c3f
khishigtsooj <Khishigtsoojbathuyg@gmail.com>
2023-03-09 12:38:46 +0800

    First Commit

---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+import p from "lodash";
+import query from "./model/Search";
+import { add as niilber, multiply as mul, superAdd } from "./view/searchView";
+
+console.log("Хайлт : " + query);
+
+console.log("Хоёр тооны нийлбэр : " + superAdd(4, 5));
